818ed6057dd988430a168abc4abd5e9bb648d07d
marco6 <mrcmnn@gmail.com>
2013-10-26 12:19:35 +0200

    Riorganizzato tutto e finito l'esercizio! AMEN!

--- Sistemi Operativi/bz/Es3/Es1.c
+++ Sistemi Operativi/bz/Es3/Es1.c
@@ -24,10 +24,19 @@ int main(int argc, char *argv[]){
 //Funzioni/variabili nascoste al main e in generale utili solo all'implementazione
 char pathBuf[MAX_PATH] = {0};
 
-void file_copy(int fdf, int fdt);
+void file_copy(int fdf, int fdt){
+  static char buff[4096];
+  int r;
+  while((r = read(fdf, buff, 4096))){
+    write(fdt, buff, r);
+  }
+  close(fdf);
+  close(fdt);
+}
 
 void dir_copy_r(struct dirent *de, int bk){
   DIR*d;
+  struct dirent *drb;
   struct stat st;
   int nln;
   if(strcmp(de->d_name, ".") && strcmp(de->d_name, "..")){
@@ -35,29 +44,33 @@ void dir_copy_r(struct dirent *de, int bk){
       puts("Errore");
       exit(1);
     }//if
+
+    //Per la ricorsione concateno le stringhe.. ovviamente in modo furbo visto che ho già la lunghezza!
+    pathBuf[bk] = '/';
+    pathBuf[bk+1]='\0';
+            
+    strcpy(pathBuf + bk + 1, de->d_name);
+      
     if(S_ISDIR(st.st_mode)){
-      //Per la ricorsione concateno le stringhe.. ovviamente in modo furbo visto che ho già la lunghezza!
-      strcat(pathBuf + bk, de->d_name);
-      
-      pathBuf[bk] = '/';
-      pathBuf[bk+1]='\0';
-      
       //creo la nuova cartella con i permessi uguali a quelli dell'originale
       mkdir(pathBuf, st.st_mode);
       //Mi calcolo la lunghezza di nln
       nln = strlen(de->d_name);
+
       //entro nella cartella
       chdir(de->d_name);
       //e la apro
       d = opendir(".");
       //Ora leggo e copio
-      while(( de = readdir(d) )) {
-	dir_copy_r(de, bk + nln+1);
-      }
+      while(( drb = readdir(d) )) {
+	dir_copy_r(drb, bk + nln+1);
+      }//while
       closedir(d);
       chdir("..");
-      pathBuf[bk] = '\0';
-    }
+    }//if
+    else
+      file_copy(open(de->d_name, O_RDONLY), open( pathBuf, O_WRONLY | O_CREAT | O_TRUNC, st.st_mode ));
+    pathBuf[bk] = '\0';
   }//if
 }//dir_copy_r
 
@@ -65,18 +78,17 @@ void dir_copy(const char*from, const char*to){
   DIR*d;
   struct dirent * de;
   //Questo mi serve per tornare indietro nel path dopo la ricorsione..
-  int bk = strlen(to);
+  int bk;
+
+  //e imposto la destinazione
+  realpath(to, pathBuf);
+  bk = strlen(pathBuf);
+  
   //Solito chdir per evitare di portarmi dietro il nome
   chdir(from);
   //E ora leggo la radice
   d = opendir(".");
   
-  //e imposto la destinazione
-  strcpy(pathBuf, to);
-
-  pathBuf[bk] = '/';
-  pathBuf[++bk]='\0';
-
   while(( de = readdir(d) )) {
     //per ogni elemento, copio.
     dir_copy_r(de, bk);
