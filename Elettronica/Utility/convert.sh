6dbadfa430d48e78f941aa8c06a0dafe98f77f53
marco6 <mrcmnn@gmail.com>
2013-12-04 15:40:58 +0100

    scritto lo script per la conversione e funge... ;) Bonato ora non avrà più scuse u.u

---
+++ Elettronica/Utility/convert.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+    folders=( "." )
+else
+    folders=( "$@" )
+fi
+
+for fold in $folders
+do
+    #cambio directory di lavoro!
+    pushd $fold > /dev/null
+    #ora becco tutti gli svg
+    for file in $(ls *.svg)
+    do
+	echo -n "Converto $file..."
+	convert -density 144 $file -gravity Center -crop 90%x73%+0+0 -trim $(echo $file | cut -f 1 -d .).jpg
+	echo " Fatto!"
+    done
+    
+    #torno dov'ero!
+    popd > /dev/null
+done
